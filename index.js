eb227232d0a89eae607ab742c083d56fc4a5f56a
R. LaRoi <are@raewhoo.com>
2023-04-08 17:45:23 +0200

    Layout

---
+++ index.js
@@ -0,0 +1,13 @@
+// const nav = document.getElementsByClassName("nav-stick");
+
+// let navSticky = nav.offsetTop;
+// function positionNav() {
+//   if (window.scrollY >= navSticky) {
+//     nav.classList.add("nav-stick");
+//     console.log("scrolling");
+//   } else {
+//     nav.classList.remove("nav-stick");
+//   }
+// }
+
+// window.addEventListener("scroll", positionNav);
